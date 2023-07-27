5a831276c4ea4cb536d68f4b975fdcff13dd7c80
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-27 13:39:34 +0100

    display todos

--- src/App.vue
+++ src/App.vue
@@ -89,6 +89,23 @@ onMounted(() => {
       </form>
     </section>
 
-    {{ todos_asc }}
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        <div
+          v-for="todo in todos_asc"
+          :class="`todo-item ${todo.done && 'done'}`"
+        >
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
