d109868362e432148d8639c92d7c673dc314f653
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-27 13:46:06 +0100

    empty inputs

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,9 @@ const addTodo = () => {
     done: false,
     createdAt: new Date().getTime(),
   });
+
+  input_content.value = "";
+  input_category.value = null;
 };
 
 const removeTodo = (todo) => {
